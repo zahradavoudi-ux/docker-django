a73289444cf29e3687aa843b997f8585b9f0a57b
zahra <zdavoudi16@gmail.com>
2024-10-04 18:12:15 +0330

    change setting.py

--- djangoProject8/settings.py
+++ djangoProject8/settings.py
@@ -164,8 +164,8 @@ ACCOUNT_LOGOUT_REDIRECT_URL = '/'  # مسیر بعد از خروج
 # -----email-------------
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'zeynabdavoudi3@gmail.com'
-EMAIL_HOST_PASSWORD = 'skxk ocbk vbfl yaxf'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_SSL = False
